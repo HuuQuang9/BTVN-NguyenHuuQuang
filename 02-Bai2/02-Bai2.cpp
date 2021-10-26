45845b59bb2468195c6e49dfc92ba858fec95cd1
HuuQuang9 <quang91120@gmail.com>
2021-10-26 16:16:50 +0800

    Complete bai2

---
+++ 02-Bai2/02-Bai2.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a, b, x;
+    cout << "giai pt ax + b =  0" << endl ;
+    cout << "a = ";
+    cin >> a; '\n';
+    cout << "b = ";
+    cin >> b; '\n';
+    x = -b / a;
+    cout << "x = " << x << endl;
+    
+}
+
