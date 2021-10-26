e443a1d27742e6c4253298bf264f6860ba17b1f9
HuuQuang9 <quang91120@gmail.com>
2021-10-26 17:00:21 +0800

    update bai 3

--- 03-Bai3/03-Bai3.cpp
+++ 03-Bai3/03-Bai3.cpp
@@ -1,20 +1,28 @@
-// 03-Bai3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    string so[100];
+    so[10] = "muoi";
+    so[11] = "muoi mot";
+    so[12] = "muoi hai";
+    so[13] = "muoi ba";
+    so[14] = "muoi bon";
+    so[15] = "muoi nam";
+    so[16] = "muoi sau";
+    so[17] = "muoi bay";
+    so[18] = "muoi tam";
+    so[19] = "muoi chin";
+    so[20] = "hai muoi";
+    so[21] = "hai mot";
+    so[22] = "hai hai";
+        
+       
+
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
