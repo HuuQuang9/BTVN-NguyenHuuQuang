f16179cea4aae41b4b4b437b3c348adecc74520d
HuuQuang9 <quang91120@gmail.com>
2021-10-29 08:26:18 +0800

    complete bai 3

--- 03-Bai3/03-Bai3.cpp
+++ 03-Bai3/03-Bai3.cpp
@@ -5,23 +5,58 @@ using namespace std;
 
 int main()
 {
-    string so[100];
-    so[10] = "muoi";
-    so[11] = "muoi mot";
-    so[12] = "muoi hai";
-    so[13] = "muoi ba";
-    so[14] = "muoi bon";
-    so[15] = "muoi nam";
-    so[16] = "muoi sau";
-    so[17] = "muoi bay";
-    so[18] = "muoi tam";
-    so[19] = "muoi chin";
-    so[20] = "hai muoi";
-    so[21] = "hai muoi mot";
-    so[22] = "hai muoi hai";
-    so[23] = "hai muoi ba";
-    so
-        
+    int a;
+    do {
+        cout << "nhap so co 2 chu so: ";
+        cin >> a;
+        if (a < 10 || a > 99) {
+            cout << "ban da nhap sai vui long thu lai" << endl;
+        }
+        else {
+            switch (a / 10) {
+            case 1 :
+                cout << "muoi"; break;
+            case 2 :
+                cout << "hai muoi"; break;
+            case 3 :
+                cout << "ba muoi"; break;
+            case 4 :
+                cout << "bon muoi"; break;
+            case 5 :
+                cout << "nam muoi"; break;
+            case 6:
+                cout << "sau muoi"; break;
+            case 7:
+                cout << "bay muoi"; break;
+            case 8:
+                cout << "tam muoi"; break;
+            case 9:
+                cout << "chin muoi"; break;
+            }
+
+            switch (a % 10) {
+            case 1:
+                cout << " mot"; break;
+            case 2:
+                cout << " hai"; break;
+            case 3:
+                cout << " ba"; break;
+            case 4:
+                cout << " bon"; break;
+            case 5:
+                cout << " nam"; break;
+            case 6:
+                cout << " sau"; break;
+            case 7:
+                cout << " bay"; break;
+            case 8:
+                cout << " tam"; break;
+            case 9:
+                cout << " chin"; break;
+            }
+
+        }
+    } while (a < 10 || a > 99);
        
 
 
