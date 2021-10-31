02f59a65c3a0ca6d462f419489d30e1203187b5f
HuuQuang9 <quang91120@gmail.com>
2021-10-31 15:19:40 +0800

    complete bai5

---
+++ 05-Bai5/05-Bai5.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int k,i=1;
+    cout << "nhap vao so nguyen k: ";
+    cin >> k;
+    do {
+        cout << i << " ";;
+        i++;
+    } while (i <= k);
+}
+
