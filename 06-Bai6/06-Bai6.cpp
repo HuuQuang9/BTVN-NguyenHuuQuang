ca8b432f076b64168cd82ef454726e293100b2f3
HuuQuang9 <quang91120@gmail.com>
2021-11-02 14:05:38 +0800

    complete all

--- 06-Bai6/06-Bai6.cpp
+++ 06-Bai6/06-Bai6.cpp
@@ -16,13 +16,38 @@ int giaithua(int x) {
 
 int main()
 {
-    int m, n, t;
-    cout << "nhap n: ";
+   
+   /* cout << "nhap n: ";
     cin >> n;
     cout << "nhap m: ";
     cin >> m;
-    t = giaithua(n) + giaithua(m);
-    cout << "(n! + m!) = " << t;
+    t = giaithua(n) + giaithua(m);*/
+    int m, n, t, mang[1000];
+    t = 0;
+    cout << "nhap n: ";
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> mang[i];
+    }
+    for (int i = 0; i < n; i++) {
+        t += giaithua(mang[i]);
+    }
+    cout << "tong = " << t << endl;
+    // xong phan tinh tong giai thua  cua mang 
+    //den phan sap xep
+    int tg;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (mang[j] < mang[i]) {
+                tg = mang[i];
+                mang[i] = mang[j];
+                mang[j] = tg;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << mang[i]<<" ";
+    }
     return 0;
 }
 
